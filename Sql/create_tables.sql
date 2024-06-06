19f845e0cd6c4dd9088c9a176dd92a94f5a89fbc
timtom2016 <dev@timtom2016.com>
2024-06-06 14:41:31 +0200

    Updated SQl

--- Sql/create_tables.sql
+++ Sql/create_tables.sql
@@ -1,49 +1,42 @@
--- check if 1 to n does change the relation
-
--- table
+-- Add up migration script here
 create table device(
-    deviceId INT AUTO_INCREMENT PRIMARY KEY,
-    name STRING(32) NOT NULL,
-    macAddr STRING(37) NOT NULL,
+    deviceId INT PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    macAddr VARCHAR(37)
 );
 
 -- table
 create table sensor(
-    sensorId INT AUTO_INCREMENT PRIMARY KEY,
-    name STRING(32) NOT NULL,
-    type STRING(32) NOT NULL,
+    sensorId INT PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    type VARCHAR(32) NOT NULL,
     pin INT,
-    addr STRING(32)
+    addr VARCHAR(32)
 );
 
 -- table
-create table sensorValue(
-    timeStamp TIMESTAMP PRIMARY KEY,
-    value FLOAT NOT NULL
+CREATE TABLE measures (
+    time_stamp TIMESTAMP NOT NULL,
+    sensorId INT NOT NULL,
+    value FLOAT NOT NULL,
+    PRIMARY KEY (sensorId, time_stamp),
+    FOREIGN KEY (sensorId) REFERENCES sensor(sensorId)
 );
 
 -- relation
 create table canSee(
-    deviceId INT NOT NULL AUTO_INCREMENT,
-    id INT NOT NULL AUTO_INCREMENT,
+    deviceId INT NOT NULL,
+    id INT NOT NULL,
     FOREIGN KEY (deviceId) REFERENCES device(deviceId),
-    FOREIGN KEY (user) REFERENCES users(id),
+    FOREIGN KEY (id) REFERENCES users(id),
     PRIMARY KEY (deviceId, id)
 );
 
 -- relation
 create table isOn(
-    deviceId INT NOT NULL AUTO_INCREMENT,
-    sensorId INT NOT NULL AUTO_INCREMENT,
+    deviceId INT NOT NULL,
+    sensorId INT NOT NULL,
     FOREIGN KEY (deviceId) REFERENCES device(deviceId),
     FOREIGN KEY (sensorId) REFERENCES sensor(sensorId),
     PRIMARY KEY (deviceId, sensorId)
-);
-
--- relation
-create table measures(
-    timeStamp TIMESTAMP NOT NULL,
-    sensorId INT NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY (sensorId) REFERENCES sensor(sensorId),
-    FOREIGN KEY (timeStamp) REFERENCES sensorValue(timeStamp),
 );
