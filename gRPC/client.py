b9d5f9161298ffaf9258c5f08ba0755965f8bf15
ezzzzClap <schuerzen.purine_0p@icloud.com>
2024-06-07 08:44:35 +0200

    Multithreating needed so that the rest of the Pogramm can continue

--- gRPC/client.py
+++ gRPC/client.py
@@ -1,3 +1,8 @@
+"""
+Dominik Fiegl 06.2024
+Test Client file to test the gRPC server.
+Not needed anymore 
+"""
 import grpc
 import server_pb2
 import server_pb2_grpc
@@ -11,11 +16,11 @@ def run():
 
         # Create a new sensor request
         response = stub.new_sensor_request(server_pb2.add_new_sensor_message(name='Sensor1', type='Type1', pin=1, addr='Addr1'))
-        print("New sensor request response received: ") # + response.message) need to add response.message in server.proto
+        print("New sensor request response received: ") # + response.message) need to add variable tp done_message in server.proto
 
         # Delete a sensor request
         response = stub.delete_sensor_request(server_pb2.delete_sensor_message(sensorId=1))
-        print("Delete sensor request response received: ") # + response.message) need to add response.message in server.proto
+        print("Delete sensor request response received: ") # + response.message) need to add variable tp done_message in server.proto
 
-
-run()
+if __name__ == '__main__':
+    run()
