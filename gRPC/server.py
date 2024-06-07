0bd9ad8cf4b4c8ed15842488eb6d23d8ce5d62b2
ezzzzClap <schuerzen.purine_0p@icloud.com>
2024-06-07 08:44:35 +0200

    Multithreating needed so that the rest of the Pogramm can continue

--- gRPC/server.py
+++ gRPC/server.py
@@ -1,8 +1,18 @@
+"""
+Dominik Fiegl 06.2024
+Program to start the gRPC server based on the server.proto file.
+The server listens on port 50051 for incoming requests.
+Is meant to be implemented to the main file of the SGAS project:
+logging.basicConfig()
+server = serve()
+threading.Thread(target=server.wait_for_termination).start()
+"""
 from concurrent import futures
 import grpc
 import logging
 import server_pb2_grpc
-import server_pb2  # Uncomment this if you're using server_pb2.done_message()
+import server_pb2  # Uncomment this line if you're using server_pb2.done_message()
+import threading
 
 class sgas_serviceServicer(server_pb2_grpc.sgas_serviceServicer):
     def new_sensor_request(self, request, context):
@@ -23,12 +33,16 @@ class sgas_serviceServicer(server_pb2_grpc.sgas_serviceServicer):
         return server_pb2.done_message()
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     server_pb2_grpc.add_sgas_serviceServicer_to_server(sgas_serviceServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    server.wait_for_termination()
+    return server
 
 if __name__ == "__main__":
     logging.basicConfig()
-    serve()
+    server = serve()
+    print("Server started")
+    print("Server listening on port 50051")
+    threading.Thread(target=server.wait_for_termination).start()
+    print("Continuing with the rest of the Python file...")
