44966ae2d0733de4690dc9c95f5e837bbcae9b44
timtom2016 <dev@timtom2016.com>
2024-06-18 13:23:15 +0200

    First

--- Website/build.rs
+++ Website/build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(feature="ssr")]
     let mut config = tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional");
     #[cfg(feature="ssr")]
-    config.compile(&["proto/server.proto"], &["proto/"])?;
+    config.compile(&["../Raspberry/server.proto"], &["../Raspberry/"])?;
     Ok(())
 }
